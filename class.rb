3d3e3f6e64320625ba07f32af2ce0bffb1e9dfc2
Gaurav <67725684+Gauravkumawat630@users.noreply.github.com>
2024-07-31 12:32:04 +0530

    add

--- class.rb
+++ class.rb
@@ -1,25 +1,25 @@
 # # #                                                           # define a class with method
 
-# # # class Gaurav
-# # #   def ram  
-# # #      puts("gauravkumawat")
-# # #    end
-# # #  def aakash
-# # #     puts ("hello aakash")
-# # #  end
-# # #     def amit
-# # #       puts ("third method")
+class Gaurav
+  def ram  
+     puts("gauravkumawat")
+   end
+ def aakash
+    puts ("hello aakash")
+ end
+    def amit
+      puts ("third method")
    
-# # #  end 
-# # # end
+ end 
+end
 
 
-# # #  object1 = Gaurav.new
-# # #  object2= Gaurav.new
-# # #  object3 = Gaurav.new
-# # #    puts (object1.ram)
-# # #    puts (object2.aakash)
-# # #    puts (object3.amit)
+ object1 = Gaurav.new
+ object2= Gaurav.new
+ object3 = Gaurav.new
+   puts (object1.ram)
+   puts (object2.aakash)
+   puts (object3.amit)
   
 
 
@@ -104,57 +104,3 @@
 # p = Proc.new{|a,b|puts a**2+b**2}
 # p.call 2,2
 
-
-# square      =  #___(x) {x**2}________________________
-# # Write a lambda which takes an integer and increment it by 1
-# plus_one    = #_____(x) {x+1}________________________________ 
-# # Write a lambda which takes an integer and multiply it by 2
-# into_2      = #__________(x) {2*x}______________________ 
-# # Write a lambda which takes two integers and adds them
-# adder       = #________(x,y){x+y}_____________________________ 
-# # Write a lambda which takes a hash and returns an array of hash values
-# values_only = #_____(h) {h.values}_______________________________ 
-# input_number_1 = gets.to_i
-# input_number_2 = gets.to_i
-# input_hash = eval(gets)
-# a = square.(input_number_1); b = plus_one.(input_number_2);c = into_2.(input_number_1); 
-# d = adder.(input_number_1, input_number_2);e = values_only.(input_hash)
-# p a; p b; p c; p d; p e
-# square      = -> (x) { x**2 }
-# # Write a lambda which takes an integer and increment it by 1
-# plus_one    = -> (x) { x + 1 }
-
-# # Write a lambda which takes an integer and multiply it by 2
-# into_2      = -> (x) {2*x}
-
-# # Write a lambda which takes two integers and adds them
-# adder       = -> (a ,b) {a + b}
-
-# # Write a lambda which takes a hash and returns an array of hash values
-# values_only = -> (h) { h. values }
-
-
-# input_number_1 = gets.to_i
-# input_number_2 = gets.to_i
-# input_hash = eval(gets)
-
-# a = square.(input_number_1); b = plus_one.(input_number_2);c = into_2.(input_number_1); 
-# d = adder.(input_number_1, input_number_2);e = values_only.(input_hash)
-
-# p a; p b; p c; p d; p e
-   
-
- # utf Unicode Transformation Format.
-
-#puts "String".bytes 
-#puts 'Hello'.bytes  
-
-# string = "surname=testsurname&name=testname"
-       
-#  puts string.split("&").sort.join("&")
-
-
-def name
-    puts self 
-  end
-  name
